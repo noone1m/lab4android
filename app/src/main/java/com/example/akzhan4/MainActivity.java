d2ab679f802292bf179c5475567024f975fa7bc5
noone1m <120911070+noone1m@users.noreply.github.com>
2024-02-16 22:51:43 +0600

    first commit

---
+++ app/src/main/java/com/example/akzhan4/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.akzhan4;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.example.akzhan4.R;
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout= findViewById(R.id.drawerlayout);
+        navigationView= findViewById(R.id.navigationview);
+        toolbar= findViewById(R.id.toolbar);
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_open, R.string.navigation_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+}
